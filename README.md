63b4b1360c722d2244cea2a5a71e56fd7e273511
Hemanth Sahukari <sahukarihemanth586@gmail.com>
2024-07-31 13:20:58 +0530

    modified

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
-# React + Vite
+1.In the Root Component (App.jsx) i have written all the logic for add,edit,delete,update and cancel buttons.
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+2.If user clicks on add button without typing anything in the input field a toast message will be displayed. i have used "react-hot-toast" for sending toast messages.
 
-Currently, two official plugins are available:
+3.If user enters their todo and click on "add" button a post request will be made to this backend route
+"http://localhost:5000/tasks" and this route will get the data entered by user and stores that data to "mongodb atlas" database.
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+4.If user clicks on "edit" button "updateTask" function will be called and it is responsible for updating the existing todo. If user changes the existing todo and clicks on "update" button a put request will be made to this route "http://localhost:5000/tasks/${id}" here "id" is the path parameter which is unique for every todo.
+
+5. If user clicks on "delete" button a delete request will be made to this route "http://localhost:5000/tasks/${id}" here "id" is the path parameter which is unique for every todo.
--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import TaskList from './components/TaskList';
 import TaskForm from './components/TaskForm';
-import SearchBar from './components/SearchBar';
 import './App.css';
 import toast, { Toaster } from 'react-hot-toast';
 import Footer from './components/Footer';
 
-const API_URL = 'https://todo-list-backend-hvyo.onrender.com/tasks';
+const API_URL = 'http://localhost:5000/tasks';
 
 function App() {
   const [tasks, setTasks] = useState([]);
   const [searchText, setSearchText] = useState('');
+
+  //THE logic inside this "useEffect" hook will send a request to backend route
+  //which is responsible for sending all the available tasks which are stored in "MongoDB Atlas" database.
+  //If user previously added any tasks then those tasks will be available in "tasks" state variable
   useEffect(() => {
     const fetchTasks = async () => {
       try {
@@ -24,6 +27,7 @@ function App() {
     fetchTasks();
   }, []);
 
+ // THIS FUNCTION IS CALLED IF USER CLICKS ON ADD BUTTON
   const addTask = async (text) => {
     try {
       const response = await fetch(API_URL, {
@@ -39,7 +43,7 @@ function App() {
       console.error(err);
     }
   };
-
+ // THIS FUNCTION WILL BE CALLED IF USER CLICKS ON UPDATE BUTTON
   const updateTask = async (id, newText) => {
     try {
       const response = await fetch(`${API_URL}/${id}`, {
@@ -56,7 +60,7 @@ function App() {
       console.error(err);
     }
   };
-
+ //THIS FUNCTION WILL BE CALLED IF USER CLICKS ON DELETE BUTTON
   const deleteTask = async (id) => {
     try {
       await fetch(`${API_URL}/${id}`, {
@@ -68,7 +72,7 @@ function App() {
       console.error(err);
     }
   };
-
+  //THIS FUNCTION WILL BE CALLED IF USER CLICKS ON TODO
   const toggleTask = async (id) => {
     const task = tasks.find(task => task._id === id);
     try {
@@ -92,7 +96,13 @@ function App() {
     <>
     <div className="app">
       <h1>Todo List</h1>
-      <SearchBar searchText={searchText} onSearch={setSearchText} />
+      <input
+      type="text"
+      value={searchText}
+      onChange={(e) => setSearchText(e.target.value)}
+      placeholder="Search tasks..."
+      className='search-tasks'
+    />
       <TaskForm onAdd={addTask} />
       <TaskList tasks={filteredTasks} onUpdate={updateTask} onDelete={deleteTask} onToggle={toggleTask}  />
       <Footer totalTodos={tasks}/>
